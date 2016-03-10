b9bac45fcc15e323ae72e0138781d9077322db8e
rockstarprogrmr <rockstarprogrmr@gmail.com>
2016-03-11 00:22:53 +0800

    Added countChar function. This function will take a string input and return the number of characters from the input. It is a customized function without using string.h library.

---
+++ StringManipulation/Source.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "stdint.h"
+#include "StringManipulation.h"
+
+int main() {
+	unsigned char simpleText[] = "Hello";
+	unsigned long numChar = 0;
+
+	numChar = countChar(simpleText);
+	printf("Number of character = %d", numChar);
+
+	return 0;
+}
---
+++ StringManipulation/StringManipulation.h
@@ -0,0 +1,15 @@
+unsigned long countChar(unsigned char *);
+
+
+//This function will count the number of characters for a given char input
+unsigned long countChar(unsigned char *charTextPointer) {
+	unsigned long charCount = 0;
+
+	//Fill counter while nth address of character input != NULL or \0
+	while (*charTextPointer != '\0'){
+		charTextPointer++;
+		charCount++;
+	}
+
+	return charCount;
+}
